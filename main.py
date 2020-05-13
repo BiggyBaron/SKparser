9eccc22aa0a0931f4e0e2b2095e41e26b7a39a37
BiggyBaron <dr.cleverest@gmail.com>
2020-05-13 23:57:55 +0600

    First check

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ import sys
 import telebot
 
 
-def dojob(data):
-    # photo = open('/home/ubuntu/SKparser/landing_page.png', 'rb')
-    chat_id = "536244426"
-    bot.send_message(chat_id, data)
-
- 
-
 base_url = ("https://backend.digital-summer.sk.kz/login")
 second_url = "https://backend.digital-summer.sk.kz/member/index?year=2020&sort=case_total"
 options2 = webdriver.ChromeOptions()
@@ -23,22 +16,28 @@ options2.add_argument('headless')
 driver = webdriver.Chrome("chromedriver", options=options2)
 bot = telebot.TeleBot("1272517220:AAGp0kXsJc7Ne7qhZudC0EuiF3z1qnUhj4Q")
 
-
-
-driver.get(base_url)
-driver.maximize_window()
-username = driver.find_element_by_id("login-form-login")
-password = driver.find_element_by_id("login-form-password")
-username.send_keys("dr.cleverest@gmail.com")
-password.send_keys("NkGAfL")
-driver.find_element_by_id("login-form").submit()
-time.sleep(10)
-driver.get(second_url)
-data = driver.find_element_by_id("w0-container")
-time.sleep(10)
-dojob(data)
-# driver.save_screenshot("/home/ubuntu/SKparser/landing_page.png")
+dojob("536244426")
 
 
 
-driver.quit()
+@bot.message_handler(content_types=['text'])
+def start_message(message):
+    dojob(chat_id)
+
+
+def dojob(chat_id):
+    driver.get(base_url)
+    driver.maximize_window()
+    username = driver.find_element_by_id("login-form-login")
+    password = driver.find_element_by_id("login-form-password")
+    username.send_keys("dr.cleverest@gmail.com")
+    password.send_keys("NkGAfL")
+    driver.find_element_by_id("login-form").submit()
+    time.sleep(10)
+    driver.get(second_url)
+    data = driver.find_element_by_id("w0-container").get_attribute('innerHTML')
+    time.sleep(10)
+    bot.send_message(chat_id, data)
+    driver.quit()
+
+bot.polling()
