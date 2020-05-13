a470e31a6ef424009d69e4eaf0bd5ccfeaaffa4d
BiggyBaron <dr.cleverest@gmail.com>
2020-05-13 23:39:11 +0600

    First check

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ bot = telebot.TeleBot("1272517220:AAGp0kXsJc7Ne7qhZudC0EuiF3z1qnUhj4Q")
 
 
 driver.get(base_url)
-        driver.maximize_window()
-        username = driver.find_element_by_id("login-form-login")
-        password = driver.find_element_by_id("login-form-password")
-        username.send_keys("dr.cleverest@gmail.com")
-        password.send_keys("NkGAfL")
-        driver.find_element_by_id("login-form").submit()
-        driver.save_screenshot("~/SKparser/landing_page.png")
-        photo = open('~/SKparser/landing_page.png', 'rb')
-        chat_id = "536244426"
-        bot.send_photo(chat_id, photo)
+    driver.maximize_window()
+    username = driver.find_element_by_id("login-form-login")
+    password = driver.find_element_by_id("login-form-password")
+    username.send_keys("dr.cleverest@gmail.com")
+    password.send_keys("NkGAfL")
+    driver.find_element_by_id("login-form").submit()
+    driver.save_screenshot("~/SKparser/landing_page.png")
+    photo = open('~/SKparser/landing_page.png', 'rb')
+    chat_id = "536244426"
+    bot.send_photo(chat_id, photo)
 
 
 driver.quit()
